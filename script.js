8fbcc59fcd2ce76b886e521091c68320f6519a25
Gabriel018 <duke.comandos@gmail.com>
2022-10-27 22:01:07 -0300

    .2

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const DisplayText = document.getElementById('text_operador')
-const frase = ["Ola seja bem vindo"]
-const frase_completa = []
-function loop(){
+const frase = ["Ola seja bem vindo!!"]
+let i = 0
+let j = 0
+let isDelete = false
+let Frase_Completa = []
+function loop () {
+   DisplayText.innerHTML = Frase_Completa.join('')
+   if (i < frase.length){
+    
+        if ( !isDelete && j <= frase[i].length){
+            Frase_Completa.push(frase[i][j])
+            j++ 
+        }     
+        if ( isDelete && j <= frase[i].length){
+            Frase_Completa.pop(frase[i][j])
+            j--
+        } 
+        if ( isDelete && j === 0){
+            Frase_Completa = []
+            isDelete = false
+            i++
+        }
+        if (i === frase.length){
+            i = 0
+            
+        }
 
+        }
+        if ( j == frase[i].length){
+            isDelete = true  
+        }
+
+    const  time = isDelete ? 50 : 100
+    setTimeout(loop, time)
 }
+loop()
