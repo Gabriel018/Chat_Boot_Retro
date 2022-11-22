f67be640f322c70a6e774d2fadf8c408a3892114
Gabriel018 <duke.comandos@gmail.com>
2022-11-21 22:06:43 -0300

    1.1

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ const imgs = document.getElementById("img")
 const img  = document.querySelectorAll("#img img")
 
 const DisplayText = document.getElementById('text_operador')
-const frase = ["Ola seja bem vindo!, vou acompanha-lo durante esta cessao "]
-const frase1 = ["<br> Boa noite!! "]
-let i = 0
-let j = 0
-let isDelete = false
-let Frase_Completa = []
 
 let dmx = 0
 
-function deletar(){
-   
-}
+
+const conversa = {Ola:"como Vai"}
+
+
+
 console.log(document.getElementById('txt_cliente').value)
 function operador_carrocel(){
     dmx++
@@ -27,86 +23,29 @@ function operador_carrocel(){
 }
    setInterval(operador_carrocel,40)
 
- console.log(dmx)
-
-function loop () {
-   DisplayText.innerHTML = Frase_Completa.join('')
-   if (i < frase.length){
-    
-        if ( !isDelete && j <= frase[i].length){
-            Frase_Completa.push(frase[i][j])
-            j++ 
-        }     
-        if ( isDelete && j <= frase[i].length){
-            Frase_Completa.pop(frase[i][j])
-            j--
-        } 
-        if ( isDelete && j === 0){
-            Frase_Completa = []
-            isDelete = false
-            i++
-        }
-        if (i === frase.length){
-            i = 0
-            
-        }
-
-        }
-        if ( j == frase[i].length){
-            Frase_Completa = [ "Boa noite"]
-            loop01()
-        }
 
 
-    const  time =  isDelete ? 100:100
-    setTimeout(loop, time)
-    
-}
-loop()
-
-function loop01 () {
-    DisplayText.innerHTML = Frase_Completa.join('')
-    if (i < frase1.length){
-     
-         if ( !isDelete && j <= frase1[i].length){
-             Frase_Completa.push(frase1[i][j])
-             j++ 
-         }     
-         if ( isDelete && j <= frase1[i].length){
-             Frase_Completa.pop(frase1[i][j])
-             j--
-         } 
-         if ( isDelete && j === 0){
-             Frase_Completa = []
-             isDelete = false
-             i++
-         }
-         if (i === frase1.length){
-             i = 0
-             
-         }
- 
-         }
-         if ( j == frase1[i].length){
-
-            isDelete = true
- 
-         }
- 
-     const  time =  isDelete ? 100:200
-     setTimeout(loop01, time)
- }
 
  //Capturar valor digitado na txtarea
 cliente.addEventListener('keyup',function(e){
     var valor = cliente.value
     console.log(valor)
     
-    if(valor == 'oi'){
+    if(valor == 'Bom dia'){
+        cliente.valor = ''
+        setTimeout(function(){
+            DisplayText.innerHTML = "Bom dia em que posso ajuda-lo"  
+        },1000)
         
 
-        Frase_Completa = [ "Boa noite"]
-           
     }
-    
-    })
+
+    if(valor == 'horas'){
+        cliente.valor = ''
+        setTimeout(function(){
+            DisplayText.innerHTML = "<br>e a hora certa"
+        },1000)
+
+    }})
+
+console.log(document.getElementById('text_operador'))
