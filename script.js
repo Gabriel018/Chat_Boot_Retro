4c31bc95d29f97a95d058495c3bff01c292c83fd
Gabriel018 <duke.comandos@gmail.com>
2022-11-22 22:55:48 -0300

    1.1

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const cliente = document.getElementById('txt_cliente')
+const cliente = document.querySelector('txt_cliente')
 const op_carrocel = document.querySelector('.op_carrocel')
 const imgs = document.getElementById("img")
 const img  = document.querySelectorAll("#img img")
 
-const DisplayText = document.getElementById('text_operador')
+const DisplayText = document.querySelector('text_operador')
 
 let dmx = 0
-
-
-const conversa = {Ola:"como Vai"}
-
 
 
 console.log(document.getElementById('txt_cliente').value)
@@ -27,25 +23,21 @@ function operador_carrocel(){
 
 
  //Capturar valor digitado na txtarea
-cliente.addEventListener('keyup',function(e){
-    var valor = cliente.value
-    console.log(valor)
+cliente.addEventListener('keyup',() => renderUserMsg())
+   const renderUserMsg = () => {
+    const texto  = cliente.value; 
+     Rendermsgele(texto);
+   }
     
-    if(valor == 'Bom dia'){
-        cliente.valor = ''
-        setTimeout(function(){
-            DisplayText.innerHTML = "Bom dia em que posso ajuda-lo"  
-        },1000)
-        
+   const Rendermsgele=(txt) => {
+    const MensengerEle = document.createElement("div")
+    const txtNode = document.createTextNode(txt)
+    MensengerEle.classList.add("use-msg")
+    MensengerEle.append(txtNode)
+    DisplayText.append(MensengerEle)
+   }
 
-    }
 
-    if(valor == 'horas'){
-        cliente.valor = ''
-        setTimeout(function(){
-            DisplayText.innerHTML = "<br>e a hora certa"
-        },1000)
 
-    }})
 
 console.log(document.getElementById('text_operador'))
